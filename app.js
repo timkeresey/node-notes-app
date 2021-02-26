aca52449b202295fb8eb5661feaa883c312dbd6a
Tim Keresey <tim.keresey@gmail.com>
2021-02-26 10:35:56 -0700

    configure add command to require title and body property when building notes

--- app.js
+++ app.js
@@ -9,8 +9,20 @@ yargs.version('1.1.0')
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
-    handler: function () {
-        console.log('Adding a new note!');
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log('Title: ' + argv.title, `Body: ${argv.body}`);
     }
 })
 
@@ -41,6 +53,4 @@ yargs.command({
     }
 })
 
-// add, remove, read, list
-
-console.log(yargs.argv);
+yargs.parse();
