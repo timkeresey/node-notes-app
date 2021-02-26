109426b87ffa36ec20a8a93acbfe5ccdacfb1bf7
Tim Keresey <tim.keresey@gmail.com>
2021-02-26 10:22:41 -0700

    create list and read commands

--- app.js
+++ app.js
@@ -23,6 +23,24 @@ yargs.command({
     }
 })
 
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'list all notes',
+    handler: function() {
+        console.log('Listing notes');
+    }
+})
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'read a note',
+    handler: function() {
+        console.log('Reading note!');
+    }
+})
+
 // add, remove, read, list
 
 console.log(yargs.argv);
